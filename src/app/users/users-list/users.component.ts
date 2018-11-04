b107e4a47f30a2b6a148d6b80c830de392cc5933
Tom McCarthy <tommccarthy@Toms-MacBook-Pro.local>
2018-11-04 13:52:29 -0500

    Updated sort function in users component to add default case.

--- src/app/users/users-list/users.component.ts
+++ src/app/users/users-list/users.component.ts
@@ -19,17 +19,19 @@ export class UsersComponent implements OnInit {
   sortUsersBy(e): void {
     if (e.nodeName === 'SPAN') {
         if (e.id === 'userName') {
-        this.users = this.users.sort((a, b) => (a.userName < b.userName) ?  -1 :  1);
+        this.users = this.users.sort((a, b) => (a.userName < b.userName) ?  -1 : (a.userName > b.userName) ? 1 : 0);
         } else if (e.id === 'firstName') {
-         this.users = this.users.sort((a, b) => (a.firstName < b.firstName) ?  -1 :  1);
+         this.users = this.users.sort((a, b) => (a.firstName < b.firstName) ?  -1 : (a.firstName > b.firstName) ? 1 : 0);
         } else if (e.id === 'lastName') {
-         this.users = this.users.sort((a, b) => (a.lastName < b.lastName) ?  -1 :  1);
+         this.users = this.users.sort((a, b) => (a.lastName < b.lastName) ?  -1 : (a.lastName > b.firstName) ? 1 : 0);
         } else if (e.id === 'birthday') {
-          this.users = this.users.sort((a, b) => (a.birthday.getTime() < b.birthday.getTime() ) ? -1 : 1);
+          this.users = this.users.sort((a, b) =>
+          (a.birthday.getTime() < b.birthday.getTime()) ? -1 : (a.birthday.getTime() > b.birthday.getTime()) ? 1 : 0);
         } else if (e.id === 'email') {
-         this.users = this.users.sort((a, b) => (a.email < b.email) ?  -1 :  1);
+         this.users = this.users.sort((a, b) => (a.email < b.email) ?  -1 :  (a.email > b.email) ? 1 : 0);
         } else if (e.id === 'date') {
-         this.users = this.users.sort((a, b) => (a.joinDate.getTime() < b.joinDate.getTime() ) ? -1 : 1);
+         this.users = this.users.sort((a, b) =>
+         (a.joinDate.getTime() < b.joinDate.getTime() ) ? -1 : (a.joinDate.getTime() > b.joinDate.getTime()) ? 1 : 0);
         }
   }
 }
